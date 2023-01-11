94471a002fa8cc70cfb440b2c2e97cbef8c9e0bc
Luis Lucero <luis.lucero@bjungle.net>
2023-01-11 09:17:53 -0500

    resolve error create table users

--- migrations/psql_20230110_150355_create_auth.users.sql
+++ migrations/psql_20230110_150355_create_auth.users.sql
@@ -3,8 +3,9 @@
 CREATE TABLE IF NOT EXISTS auth.users(
     id uuid NOT NULL PRIMARY KEY,
     type_document VARCHAR (50) NOT NULL,
-    document_number BIGINT  NOT NULL,
+    document_number BIGINT  NOT NULL UNIQUE,
     expedition_date TIMESTAMP  NOT NULL,
+    email varchar (50)  NOT NULL UNIQUE,
     first_name VARCHAR (50) NOT NULL,
     second_name VARCHAR (50) ,
     second_surname VARCHAR (50) NOT NULL,
